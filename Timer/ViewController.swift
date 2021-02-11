3e94279897c263eac6e250c4fc774dd7b2e32656
Егор Никитин <Egornik1994@yandex.ru>
2021-02-11 15:07:04 +0300

    Task completed

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var updateColorTime: Int = 10
+    
+    private let colors: [UIColor] = [.green, .blue, .cyan, .orange, .darkGray, .systemPink, .yellow, .purple, .red, .magenta]
+    
+    private var iteration: Int = 0
+    
+    private let constInfo: String = "До обновления цвета view осталось: "
+    
+    @IBOutlet private var infoLabel: UILabel! {
+        didSet{
+            infoLabel.text = "\(constInfo)\(String(updateColorTime))"
+        }
+    }
+    
+    @IBOutlet private var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super .viewDidAppear(animated)
+        updateColor()
+    }
+    
+    private func updateColor() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            
+            self.updateColorTime -= 1
+            self.infoLabel.text = "\(self.constInfo)\(String(self.updateColorTime))"
+            
+            
+            if self.updateColorTime < 0 {
+                self.infoLabel.text = "\(self.constInfo)0"
+            }
+            
+            if self.updateColorTime == 0 {
 
+                UIView.animate(withDuration: 1) {
+                    self.colorView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                } completion: { (bool) in
+                    UIView.animate(withDuration: 1) {
+                        self.colorView.transform = .identity
+                    }
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.colorView.backgroundColor = self.colors[self.iteration]
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.updateColorTime = 10
+                    self.infoLabel.text = "\(self.constInfo)\(String(self.updateColorTime))"
+                }
+                
+                if self.iteration == self.colors.count - 1 {
+                    self.iteration = 0
+                } else {
+                    self.iteration += 1
+                }
 
+           }
+        }
+    }
+    
 }
 
